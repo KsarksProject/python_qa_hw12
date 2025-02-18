58640aa7980ca4a2822759e229c38f64ec0f894b
abardashevich <abardashevich@gmail.com>
2025-02-18 23:16:43 +0100

    ya.ru

--- tests/tets_simple.py
+++ tests/tets_simple.py
@@ -1,7 +1,8 @@
 import unittest
-
+import allure
 
 class TestSimplePass(unittest.TestCase):
+    @allure.title("Успешное заполнение формы")
     def test_pass(self):
         self.assertTrue(True)
 
